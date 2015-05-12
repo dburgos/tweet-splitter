d5847e70c51641852ee1f5d5c0b0a34c40040d6a
dburgos <david.burgos.risco@gmail.com>
2015-05-12 21:00:01 +0200

    Add the eventSystem as intercommunicator between isolated components

---
+++ js/eventSystem.js
@@ -0,0 +1,32 @@
+/*
+https://gist.github.com/Yitsushi/e3b7823f7d4bf34faa4f
+Author: https://gist.github.com/Yitsushi
+*/
+var EventSystem = (function() {
+  var self = this;
+
+  self.queue = {};
+
+  return {
+    publish: function (event, data) {
+      var queue = self.queue[event];
+
+      if (typeof queue === 'undefined') {
+        return false;
+      }
+
+      while(queue.length > 0) {
+        (queue.shift())(data);
+      }
+
+      return true;
+    },
+    subscribe: function(event, callback) {
+      if (typeof self.queue[event] === 'undefined') {
+        self.queue[event] = [];
+      }
+
+      self.queue[event].push(callback);
+    }
+  };
+}());
