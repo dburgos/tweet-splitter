eefd5b6030f02476269658647d8133eec6d98c26
dburgos <david.burgos.risco@gmail.com>
2015-05-13 09:24:07 +0200

    Fix list render, only when data exists

--- js/components/tweetList.jsx
+++ js/components/tweetList.jsx
@@ -23,13 +23,20 @@ var TweetList = React.createClass({
   },
 
   render: function() {
-    return <ul className="tweets">
-        {
-          data.tweets.map(function(tweet) {
-            return <li key={tweet.id}>{tweet.text}</li>
-          })
-        }
-      </ul>
+    var data = this.state.data;
+    if (data) {
+      var hasTweets = data.tweets && data.tweets.length > 0;
+      if (hasTweets) {
+        return <ul className="tweets">
+            {
+              data.tweets.map(function(tweet) {
+                return <li key={tweet.id}>{tweet.text}</li>
+              })
+            }
+          </ul>
+      }
+    }
+    return false;
   }
 });
 
