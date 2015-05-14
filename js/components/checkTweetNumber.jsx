11eba895e59e7d2faa8307aaf7166b9200bc1396
dburgos <david.burgos.risco@gmail.com>
2015-05-14 15:27:40 +0200

    Add new component: option check to show the tweet number

---
+++ js/components/checkTweetNumber.jsx
@@ -0,0 +1,31 @@
+/** @jsx React.DOM */
+var CheckTweetNumber = React.createClass({
+  getInitialState: function() {
+    return {
+      checked: false
+    };
+  },
+
+  handleChange: function(e) {
+    EventSystem.publish('input.check.show', {
+      key:   "showNumber",
+      value: e.target.checked
+    });
+    this.setState({ checked: e.target.checked });
+  },
+
+  render: function() {
+    return  <label className="checkbox">
+              <input
+                type="checkbox"
+                checked={this.state.checked}
+                onChange={this.handleChange} />
+              <span>Show tweet number</span>
+            </label>;
+  }
+});
+
+React.render(
+  <CheckTweetNumber />,
+  document.getElementById("options")
+);
